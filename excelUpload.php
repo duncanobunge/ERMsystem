3fffacfd1a74801db061a3ee02b6ea92b1dec21c
duncanobunge <dancunobunge@gmail.com>
2019-05-22 16:08:07 +0300

    excelupload modified

--- excelUpload.php
+++ excelUpload.php
@@ -22,33 +22,44 @@ $totalSheet = count($Reader->sheets());
 echo "You have total ".$totalSheet." sheets";
 
 $html="<table border='1'>";
-$html.="<tr><th>Item Name</th><th>Description</th><th>P.O. No.</th><th>Quantity</th><th>Delivery Date</th>
-<th>Receiver</th></tr>";
+$html.="<tr>
+<th>Item Description</th>
+<th>Delivery Date</th>
+<th>Supplier</th>
+<th>P.O No</th>
+<th>Qty Received</th>
+<th>UoM</th>
+<th>Remarks</th></tr>";
 
 /* For Loop for all sheets */
-for($i=1;$i<$totalSheet;$i++)
+for($i=0;$i<$totalSheet;$i++)
 {
 $Reader->ChangeSheet($i);
         foreach ($Reader as $Row)
         {
         $html.="<tr>";
-        $item_name = isset($Row[0]) ? $Row[0] : '';
-        $item_description = isset($Row[1]) ? $Row[1] : '';
-        $po_number = isset($Row[2]) ? $Row[2] : '';
-        $quantity = isset($Row[3]) ? $Row[3] : '';
-        $delivery_date = isset($Row[4]) ? $Row[4] : '';
-        $receiver_name = isset($Row[5]) ? $Row[5] : '';
-        $html.="<td>".$item_name."</td>";
+        $item_description=isset($Row[0]) ? $Row[0] : '';
+        $delivery_date = isset($Row[1]) ? $Row[1] : '';
+        $item_supplier=isset($Row[2]) ? $Row[2] : '';
+        $po_number = isset($Row[3]) ? $Row[3] : '';
+        $quantity = isset($Row[4]) ? $Row[4] : '';
+        $uom = isset($Row[5]) ? $Row[5] : '';
+        $remarks = isset($Row[6]) ? $Row[6] : '';
         $html.="<td>".$item_description."</td>";
+        $html.="<td>".$delivery_date."</td>";
+        $html.="<td>".$item_supplier."</td>";
         $html.="<td>".$po_number."</td>";
         $html.="<td>".$quantity."</td>";
-        $html.="<td>".$delivery_date."</td>";
-        $html.="<td>".$receiver_name."</td>";
+        $html.="<td>".$uom."</td>";
+        $html.="<td>".$remarks."</td>";
         $html.="</tr>";
 
-        $query = "INSERT INTO delivereditems_tbl(item_id,item_name,item_description,po_number,quantity,delivery_date,receiver_name,creater_id,creation_date) 
-                VALUES('','$item_name','$item_description','$po_number','$quantity','$delivery_date','$receiver_name','',CURRENT_TIMESTAMP)";
-                mysqli_query($con,$query) or die(mysqli_error($con));
+       // echo  $item_description;
+       // echo  $delivery_date;
+
+        $query="INSERT INTO delivereditems_tbl(item_id,item_supplier,item_description,po_number,quantity,uom,delivery_date,remarks,creater_id,creation_date)
+                VALUES('','$item_supplier','$item_description','$po_number','$quantity','$uom','$delivery_date','$remarks','4',CURRENT_TIMESTAMP)";
+   mysqli_query($con,$query) or die(mysqli_error($con));
                 
         }
 }
